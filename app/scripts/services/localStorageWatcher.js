39393ae15419c5168c3997bb643c26c400d578b1
Alexander Ulanouski <alexander.ulanouski@gmail.com>
2014-08-11 10:59:12 +0200

    fixed jshint errors

--- app/scripts/services/localStorageWatcher.js
+++ app/scripts/services/localStorageWatcher.js
@@ -1,25 +1,27 @@
+'use strict';
+
 angular.module('techRadarApp').factory('localStorageWatcher', [
-  '$log', '$rootScope', 'localStorage', function ($log, $rootScope, localStorage) {
+  '$log',
+  '$rootScope',
+  'localStorage',
+  function ($log, $rootScope, localStorage) {
     var syncWithLocalStorage;
     syncWithLocalStorage = function (storageKey, optionalObjectTemplate) {
       var objectTemplate, storageObject, storageObjectString;
       objectTemplate = optionalObjectTemplate ? optionalObjectTemplate : {};
-
       /* Initialize the sync by loading the object from local storage to be watched in the $rootScope */
       storageObjectString = localStorage[storageKey];
-      storageObject = (storageObjectString ? JSON.parse(storageObjectString) : objectTemplate);
+      storageObject = storageObjectString ? JSON.parse(storageObjectString) : objectTemplate;
       $rootScope.$watch(function () {
         /* If this object changes then the listener will be called to update local storage */
         return storageObject;
       }, function () {
-          /* Save changes to local storage */
-          return localStorage[storageKey] = JSON.stringify(storageObject);
-      /* Compare object for equality rather than for reference */
+        /* Save changes to local storage */
+        localStorage[storageKey] = JSON.stringify(storageObject);
+        return localStorage[storageKey];  /* Compare object for equality rather than for reference */
       }, true);
       return storageObject;
     };
-    return {
-      syncWithLocalStorage: syncWithLocalStorage
-    };
+    return { syncWithLocalStorage: syncWithLocalStorage };
   }
 ]);
